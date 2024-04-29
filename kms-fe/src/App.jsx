d1ea1b665779661e7547998caa6add870460f364
fahriel <fahrielfaldi@gmail.com>
2024-04-29 14:36:14 +0700

    Fix bug import reference from template

--- kms-fe/src/App.jsx
+++ kms-fe/src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Cookies from "js-cookie";
+import { decryptId } from "./component/util/Encryptor";
+import { ROOT_LINK } from "./component/util/Constants";
+import CreateMenu from "./component/util/CreateMenu";
+import CreateRoute from "./component/util/CreateRoute.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Container from "./component/backbone/Container";
+import Header from "./component/backbone/Header";
+import SideBar from "./component/backbone/SideBar";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import Login from "./component/page/login/Index";
+import Logout from "./component/page/logout/Index";
+import NotFound from "./component/page/not-found/Index";
+
+export default function App() {
+  const [listMenu, setListMenu] = useState([]);
+  const [listRoute, setListRoute] = useState([]);
+  const isLoginPage = window.location.pathname === "/login";
+  const isLogoutPage = window.location.pathname === "/logout";
+  const cookie = Cookies.get("activeUser");
+
+  if (isLoginPage) return <Login />;
+  else if (isLogoutPage) return <Logout />;
+  else if (!cookie) window.location.href = "/login";
+  else {
+    const userInfo = JSON.parse(decryptId(cookie));
+
+    useEffect(() => {
+      const getMenu = async () => {
+        const menu = await CreateMenu(userInfo.role);
+        const route = CreateRoute.filter((routeItem) => {
+          const pathExistsInMenu = menu.some((menuItem) => {
+            if (menuItem.link.replace(ROOT_LINK, "") === routeItem.path) {
+              return true;
+            }
+            if (menuItem.sub && menuItem.sub.length > 0) {
+              return menuItem.sub.some(
+                (subItem) =>
+                  subItem.link.replace(ROOT_LINK, "") === routeItem.path
+              );
+            }
+            return false;
+          });
+
+          return pathExistsInMenu;
+        });
+
+        route.push({
+          path: "/*",
+          element: <NotFound />,
+        });
+
+        setListMenu(menu);
+        setListRoute(route);
+      };
+
+      getMenu();
+    }, []);
+
+    return (
+      <>
+        {listRoute.length > 0 && (
+          <>
+            <Header displayName={userInfo.nama} roleName={userInfo.peran} />
+            <div style={{ marginTop: "70px" }}></div>
+            <div className="d-flex flex-row">
+              <SideBar listMenu={listMenu} />
+              <Container>
+                <RouterProvider router={createBrowserRouter(listRoute)} />
+              </Container>
+            </div>
+          </>
+        )}
+      </>
+    );
+  }
 }
-
-export default App
