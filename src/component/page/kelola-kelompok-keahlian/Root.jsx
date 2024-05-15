c25f4ab72196f601b502f69ee030505626154ccd
rvalf <revalinaf180@gmail.com>
2024-05-15 12:44:20 +0700

    ketinggalan

--- src/component/page/kelola-kelompok-keahlian/Root.jsx
+++ src/component/page/kelola-kelompok-keahlian/Root.jsx
@@ -1,9 +1,51 @@
-import KKIndex from "./Index";
+// import KKIndex from "./Index";
 
-export default function KelolaKelompokKeahlian() {
-  return (
-    <div>
-      <KKIndex />
-    </div>
-  );
+// export default function KelolaKelompokKeahlian() {
+//   return (
+//     <div>
+//       <KKIndex />
+//     </div>
+//   );
+// }
+
+import { useState } from "react";
+import KKIndex from "./Index";
+import KKAdd from "./Add";
+import KKEdit from "./Edit";
+import KKDetailDraft from "./DetailDraft";
+import KKDetailPublish from "./DetailPublish";
+
+export default function KelompokKeahlian() {
+  const [pageMode, setPageMode] = useState("index");
+  const [dataID, setDataID] = useState();
+
+  function getPageMode() {
+    switch (pageMode) {
+      case "index":
+        return <KKIndex onChangePage={handleSetPageMode} />;
+      case "add":
+        return <KKAdd onChangePage={handleSetPageMode} />;
+      case "detailDraft":
+        return (
+          <KKDetailDraft onChangePage={handleSetPageMode} withID={dataID} />
+        );
+      case "detailPublish":
+        return (
+          <KKDetailPublish onChangePage={handleSetPageMode} withID={dataID} />
+        );
+      case "edit":
+        return <KKEdit onChangePage={handleSetPageMode} withID={dataID} />;
+    }
+  }
+
+  function handleSetPageMode(mode) {
+    setPageMode(mode);
+  }
+
+  function handleSetPageMode(mode, withID) {
+    setDataID(withID);
+    setPageMode(mode);
+  }
+
+  return <div>{getPageMode()}</div>;
 }
