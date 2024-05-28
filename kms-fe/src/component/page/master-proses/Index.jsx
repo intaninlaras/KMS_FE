8a59c458295601ea6533f069e62ca7b54e360600
ika <ikaauliaputri5@gmail.com>
2024-05-28 10:15:00 +0700

    status on off materi

--- kms-fe/src/component/page/master-proses/Index.jsx
+++ kms-fe/src/component/page/master-proses/Index.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useRef, useState } from "react";
-import { PAGE_SIZE } from "../../util/Constants";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import SweetAlert from "../../util/SweetAlert";
 import Button from "../../part/Button";
 import Input from "../../part/Input";
 import Paging from "../../part/Paging";
@@ -7,22 +8,21 @@ import Filter from "../../part/Filter";
 import DropDown from "../../part/Dropdown";
 import Alert from "../../part/Alert";
 import Loading from "../../part/Loading";
-import '@fortawesome/fontawesome-free/css/all.css';
-import axios from "axios";
-import SweetAlert from "../../util/SweetAlert";
 import CardMateri from "../../part/CardMateri";
+import UseFetch from "../../util/UseFetch";
+import { API_LINK } from "../../util/Constants";
+import '@fortawesome/fontawesome-free/css/all.css';
 
 const inisialisasiData = [
   {
     Key: null,
     No: null,
-    "Nama Materi": null,
+    "Kategori": null,
     Judul: null,
     File: null,
     Keterangan: null,
     "Kata Kunci": null,
     Gambar: null,
-    Uploader: null,
     Creadate: null,
     Status: "Aktif",
     Count: 0,
@@ -42,7 +42,6 @@ const dataFilterJenis = [
 ];
 
 const dataFilterStatus = [
-  
   { Value: "Aktif", Text: "Aktif" },
   { Value: "Tidak Aktif", Text: "Tidak Aktif" },
 ];
@@ -55,72 +54,58 @@ export default function MasterProsesIndex({ onChangePage }) {
   const [status, setStatus] = useState({});
   const [currentFilter, setCurrentFilter] = useState({
     page: 1,
-    query: "",
-    sort: "JUDUL",
     status: "Aktif",
+    query: "",
+    sort: "Judul",
     order: "asc",
-    
   });
-
-
+  // useEffect(() => {
+  //   console.log("filter : "+JSON.stringify(currentFilter));
+  // })
   const searchQuery = useRef(null);
   const searchFilterSort = useRef(null);
   const searchFilterStatus = useRef(null);
 
-  async function handleSetStatus(id) {
+  function handleSetStatus(id) {
     setIsError(false);
     console.log("Index ID: " + id);
-  
+
     SweetAlert(
       "Konfirmasi",
-      "Apakah Anda yakin ingin mengubah status data Materi?",
+      "Apakah Anda yakin ingin mengubah status data Pustaka?",
       "warning",
       "Ya",
     ).then((confirmed) => {
       if (confirmed) {
-        // Panggil endpoint API untuk mengubah status
-        axios.post(`http://localhost:8080/Materis/setStatusMateri`, {
-          idMateri: id,
+        UseFetch(API_LINK + "Materis/setStatusMateri", {
+          mat_id: id,
         })
-        .then((response) => {
-          // Periksa respons dari endpoint API
-          if (response.data === "SUCCESS") {
-            // Jika berhasil, update status di state
-            setStatus((prevStatus) => ({
-              ...prevStatus,
-              [id]: status[id] === "Aktif" ? "Tidak Aktif" : "Aktif",
-            }));
-            // Tampilkan pemberitahuan sukses
-            SweetAlert(
-              "Sukses",
-              `Status data Materi berhasil diubah menjadi ${status[id] === "Aktif" ? "Tidak Aktif" : "Aktif"}`,
-              "success"
-            );
-          } else {
-            // Jika gagal, tampilkan pesan error
-            setIsError(true);
-            console.error("Failed to update status");
-          }
-        })
-        .catch((error) => {
-          // Tangani kesalahan dalam pemanggilan API
-          setIsError(true);
-          console.error("Error updating status:", error);
-        })
-        .finally(() => setIsLoading(false));
+          .then((data) => {
+            if (data === "ERROR" || data.length === 0) setIsError(true);
+            else {
+              SweetAlert(
+                "Sukses",
+                "Status data Materi berhasil diubah menjadi " + data[0].Status,
+                "success"
+              );
+              handleSetCurrentPage(currentFilter.page);
+            }
+          })
+          .then(() => setIsLoading(false));
       } else {
         console.log("Operasi dibatalkan.");
       }
     });
   }
-  
 
   function handleSetCurrentPage(newCurrentPage) {
     setIsLoading(true);
-    setCurrentFilter((prevFilter) => ({
-      ...prevFilter,
-      page: newCurrentPage,
-    }));
+    setCurrentFilter((prevFilter) => {
+      return {
+        ...prevFilter,
+        page: newCurrentPage,
+      };
+    });
   }
 
   function handleSearch() {
@@ -153,25 +138,70 @@ export default function MasterProsesIndex({ onChangePage }) {
     const fetchData = async () => {
       setIsError(false);
       setIsLoading(true);
-  
+
+      // console.log("Filter: " + JSON.stringify(currentFilter));
       try {
-        const response = await axios.post("http://localhost:8080/Materis/GetDataMateri", {
-        page: currentFilter.page,
-        status: currentFilter.status,
-        query: currentFilter.query, // Kirim nilai pencarian ke server
-        orderby: 'Judul',
-        sort: currentFilter.order,
-        });
-  
-        const promises = response.data.map(async (value) => {
-          const gambarURL = await fetchGambar(value.Gambar);
-          const fileURL = await fetchGambar(value.File);
-          return { ...value, Gambar: gambarURL, File: fileURL };
-        });
-  
-        const updatedData = await Promise.all(promises);
-        setCurrentData(updatedData);
-        setFilteredData(updatedData);
+        const data = await UseFetch(
+          API_LINK + "Materis/GetDataMateri",
+          currentFilter
+        );
+        //  console.log("Fetched data:", data);
+
+        if (data === "ERROR") {
+          setIsError(true);
+        } else if (data.length === 0) {
+          setCurrentData(inisialisasiData);
+        } else {
+          const formattedData = data.map((value) => ({
+            ...value,
+          }));
+          // console.log("Formatted data:", formattedData);
+
+          const promises = formattedData.map((value) => {
+            const filePromises = [];
+
+            if (value["Gambar"]) {
+              const gambarPromise = fetch(
+                API_LINK +
+                `Utilities/Upload/DownloadFile?namaFile=${encodeURIComponent(
+                  value["Gambar"]
+                )}`
+              )
+                .then((response) => response.blob())
+                .then((blob) => {
+                  const url = URL.createObjectURL(blob);
+                  // console.log("Gambar URL:", url);
+                  value.gbr = value.Gambar;
+                  value.Gambar = url; // Simpan URL blob di objek value
+                  return value;
+                  // return { ...value, Gambar: url };
+                })
+                .catch((error) => {
+                  console.error("Error fetching gambar:", error);
+                  return value;
+                });
+              filePromises.push(gambarPromise);
+            }
+
+            return Promise.all(filePromises).then((results) => {
+              // console.log("Contents of filePromises:", filePromises);
+              // console.log("Results of fetching files:", results);
+              const updatedValue = results.reduce((acc, curr) => ({ ...acc, ...curr }), value);
+              // console.log("Updated value with blobs:", updatedValue);
+              return updatedValue;
+            });
+
+          });
+
+          Promise.all(promises)
+            .then((updatedData) => {
+              console.log("Updated data with blobs:", updatedData);
+              setCurrentData(updatedData);
+            })
+            .catch((error) => {
+              // console.error("Error updating currentData:", error);
+            });
+        }
       } catch (error) {
         setIsError(true);
         console.error("Fetch error:", error);
@@ -179,110 +209,96 @@ export default function MasterProsesIndex({ onChangePage }) {
         setIsLoading(false);
       }
     };
-  
+
     fetchData();
   }, [currentFilter]);
-  
-  async function fetchGambar(filename) {
-    if (!filename) return null; // Jika tidak ada nama file, kembalikan null
-    try {
-      const response = await fetch(
-        `http://localhost:8080/Utilities/Upload/DownloadFile?namaFile=${encodeURIComponent(filename)}`
-      );
-      if (!response.ok) throw new Error("Gambar tidak ditemukan");
-      const blob = await response.blob();
-      return URL.createObjectURL(blob);
-    } catch (error) {
-      console.error("Error fetching gambar:", error);
-      return null;
-    }
-  }
-  
-
 
   return (
-    <>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            {isError && (
-              <div className="flex-fill">
-                <Alert
-                  type="warning"
-                  message="Terjadi kesalahan: Gagal mengambil data materi."
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12">
+          {isError && (
+            <div className="flex-fill">
+              <Alert
+                type="warning"
+                message="Terjadi kesalahan: Gagal mengambil data materi."
+              />
+            </div>
+          )}
+          <div className="flex-fill">
+            <div className="input-group">
+              <Button
+                iconName="add"
+                classType="success"
+                title="Tambah Materi"
+                label="Add Course"
+                onClick={() => onChangePage("pretestAdd")}
+              />
+              <Input
+                ref={searchQuery}
+                forInput="pencarianMateri"
+                placeholder="Search"
+              />
+              <Button
+                iconName="search"
+                classType="primary px-4"
+                title="Cari"
+                onClick={handleSearch}
+              />
+              <Filter>
+                <DropDown
+                  ref={searchFilterSort}
+                  forInput="ddUrut"
+                  label="Urut Berdasarkan"
+                  type="none"
+                  arrData={dataFilterSort}
+                  defaultValue="[Judul] ASC"
+                  onChange={handleSortChange}
+                />
+                <DropDown
+                  ref={searchFilterStatus}
+                  forInput="ddStatus"
+                  label="Status"
+                  type="semua"
+                  arrData={dataFilterStatus}
+                  defaultValue="Aktif"
+                  onChange={handleStatusChange}
+                />
+              </Filter>
+            </div>
+          </div>
+          <div className="mt-3">
+          {isLoading ? (
+          <Loading />
+            ) : (
+              <div className="row">
+                <CardMateri
+                  materis={currentData}
+                  onDetail={onChangePage}
+                  onEdit={onChangePage}
+                  onStatus={handleSetStatus}
                 />
               </div>
             )}
-            <div className="flex-fill">
-              <div className="input-group">
-                <Button
-                  iconName="add"
-                  classType="success"
-                  title="Tambah Materi"
-                  label="Add Course"
-                  onClick={() => onChangePage("pretestAdd")}
-                />
-                <Input
-                  ref={searchQuery}
-                  forInput="pencarianMateri"
-                  placeholder="Search"
-                />
-                <Button
-                  iconName="search"
-                  classType="primary px-4"
-                  title="Cari"
-                  onClick={handleSearch}
-                />
-                <Filter>
-                  <DropDown
-                    ref={searchFilterSort}
-                    forInput="ddUrut"
-                    label="Urut Berdasarkan"
-                    type="none"
-                    arrData={dataFilterSort}
-                    defaultValue="[Judul] ASC"
-                    onChange={handleSortChange}
-                  />
-                  <DropDown
-                    ref={searchFilterStatus}
-                    forInput="ddStatus"
-                    label="Status"
-                    type="semua"
-                    arrData={dataFilterStatus}
-                    defaultValue="Aktif"
-                    onChange={handleStatusChange}
-                  />
-                </Filter>
 
-              </div>
-            </div>
-            <div className="mt-3">
-              {isLoading ? (
-                <Loading />
-              ) : (
-                <div className="row">
-                  {currentData.map((item) => (
-                    <CardMateri
-                      key={item.Key}
-                      item={item}
-                      onChangePage={onChangePage}
-                      handleSetStatus={handleSetStatus}
-                    />
-                  ))}
-                </div>
-              )}
-            </div>
-            
+            {currentData.length > 0 && currentData[0].Count > 20 && (
+              <Paging
+                totalItems={currentData[0].Count}
+                itemsPerPage={20}
+                currentPage={currentFilter.page}
+                onPageChange={handleSetCurrentPage}
+              />
+            )}
           </div>
         </div>
-        <div className="float my-4 mx-1">
-          <Button
-            classType="outline-secondary me-2 px-4 py-2"
-            label="Back"
-            onClick={() => onChangePage("kk")}
-          />
-        </div>
       </div>
-    </>
+      <div className="float my-4 mx-1">
+        <Button
+          classType="outline-secondary me-2 px-4 py-2"
+          label="Back"
+          onClick={() => onChangePage("kk")}
+        />
+      </div>
+    </div>
   );
 }
